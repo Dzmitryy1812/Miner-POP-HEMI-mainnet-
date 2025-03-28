d7d9699415a27bcf2b288613b876b7180c69d551
Dzmitry Mishakou <111795652+Dzmitryy1812@users.noreply.github.com>
2025-03-28 12:26:48 +0300

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -33,7 +33,7 @@ monitor_gas_and_stop_miner() {
 # Ожидание запуска майнера
 while ! pgrep -f "popmd" > /dev/null; do
     echo "Майнер не запущен. Ожидаем запуск..."
-    sleep 60
+    sleep 30
 done
 
 # Запуск мониторинга газа в фоновом режиме
